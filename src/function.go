3bdf6ff0bd28af19517fefe4c1f9d415150a04a1
Josep Perna Montane <2325566p@student.gla.ac.uk>
2020-12-09 19:18:51 +0000

    add logging to function

--- src/function.go
+++ src/function.go
@@ -1,20 +1,23 @@
-package function
+package main
 
 import (
 	"github.com/credit-to-debit/truelayer"
 	"github.com/credit-to-debit/starling"
-	"net/http"
+	// "net/http"
 	"fmt"
 )
 
-func Function(http.ResponseWriter, *http.Request) {
+// func Function(http.ResponseWriter, *http.Request) {
+func main(){
 	transactions := truelayer.GetTransactions()
-	fmt.Println("%v", transactions)
+	fmt.Printf("%v\n", transactions)
 	accountID := starling.GetAccount()
 	potID := starling.GetPot(accountID)
 	for _, transaction := range transactions.TransactionList{
-		fmt.Println("Added %f to goal", transaction.Amount)
-		starling.AddMoneyToGoal(int(transaction.Amount * 100), "GBP", accountID, potID)
+		if(transaction.Amount > 0){
+			fmt.Printf("Added %f to goal\n", transaction.Amount)
+			starling.AddMoneyToGoal(int(transaction.Amount * 100), "GBP", accountID, potID)
+		}
 	}
 
 }
